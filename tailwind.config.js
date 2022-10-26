d3cd0fe13d5cb426bf6e98a677520d3bf65e51ad
Confidence Madiope <confidencem97@gmail.com>
2022-10-26 11:43:18 +0200

    ADD: nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-blue": "hsl(209, 23%, 22%)",
+        "dark-blue": "hsl(207, 26%, 17%)",
+        "very-dark-blue": "hsl(200, 15%, 8%)",
+        "dark-gray": "hsl(0, 0%, 52%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
